d7337e849044c9082e6f17f0e01b7bf6c1cc1261
Oskari Palojärvi <oskari.akkanen@aalto.fi>
2016-12-21 18:28:35 +0200

    Created linked list for storing tanks and started to implement some box2D stuff

---
+++ Tankkipeli/src/LinkedList/List.scala
@@ -0,0 +1,59 @@
+package LinkedList
+
+import scala.reflect.ClassTag
+
+object List {
+  
+  def empty[ElementType] = new List[ElementType](Vector[ElementType]())
+  
+  def apply[ElementType](items: ElementType*): List[ElementType] = new List[ElementType](items.toVector)
+}
+
+final class List[ElementType](items: Vector[ElementType]) {
+  
+  private var firstItem: ListItem[ElementType] = NoItem[ElementType]
+  private var lastItem: ListItem[ElementType] = NoItem[ElementType]
+  private var currentItem: ListItem[ElementType] = NoItem[ElementType]
+  
+  items.foreach(x => this.append(x))
+  
+  def append(item: ElementType) = {
+    val tmp = new Item[ElementType](item, NoItem[ElementType])
+    this.firstItem match {
+      case _ : NoItem[ElementType] => {
+        this.firstItem = tmp
+        this.currentItem = tmp
+        this.lastItem = tmp
+      }
+      case _ : Item[ElementType] => {
+        this.lastItem.setNext(tmp)
+        this.lastItem = tmp
+      }
+    }
+  }
+  
+  def nextItem() ={
+    this.currentItem = this.currentItem.next
+    if (this.currentItem == NoItem[ElementType]) 
+      this.currentItem = this.firstItem
+  }
+  
+  def current(): Option[ElementType] = this.currentItem.value
+}
+
+private sealed trait ListItem[ItemType] {
+  def value:Option[ItemType]
+  def next:ListItem[ItemType]
+  def setNext(item:ListItem[ItemType]): Unit
+}
+
+private final case class NoItem[ItemType]() extends ListItem[ItemType] {
+  def value = None
+  def next = NoItem[ItemType]
+  def setNext(item: ListItem[ItemType]) = ()
+}
+private final case class Item[ItemType](private val content: ItemType, private var nextItem: ListItem[ItemType]) extends ListItem[ItemType] {
+  def value = Some(content)
+  def next = this.nextItem
+  def setNext(item: ListItem[ItemType]) = this.nextItem = item
+}
--- Tankkipeli/src/gameEngine/World.scala
+++ Tankkipeli/src/gameEngine/World.scala
@@ -2,15 +2,25 @@ package gameEngine
 
 import org.jbox2d.common.Vec2
 import org.jbox2d.dynamics
+import LinkedList.List
+
+object World {
+  val VELCITER = 6
+  val POSITER = 2
+}
 
 
-class World (val width: Int, val heigth: Int, gravity: Double) {
+class World (width: Int, heigth: Int, gravity: Double) {
+  
+  val worldWidth = Conversion.toBox2DXcoords(width)
+  val worldHeigth = Conversion.toBox2DYcoords(heigth)
   
   //creating new Box2D world to run this game simulation
   val b2gravity = Conversion.convertGravity(gravity)
   val b2world = new dynamics.World(b2gravity)
   
-  
+  //creating tank list
+  val tankList = List.empty[Tank]
   
   /**creates floor from given vector of Y-axis coordinates*/
   def setFloor(coordinates: Vector[Int]) = ???
@@ -19,18 +29,20 @@ class World (val width: Int, val heigth: Int, gravity: Double) {
   def setCeiling(coordinates: Vector[Int]) = ???
   
   /**creates tank with given id and start position, inserts it to world*/
-  def createTank(id: String, yPos: Int):Tank = ???
+  def createTank(id: String, xPos: Int):Tank = ???
   
   /**defining size of tank body objects*/
-  def setTankSize(xSize: Int, ySize: Int): Unit = ???
+  def setTankSize(xSize: Int, ySize: Int): Unit = {
+    Tank.setSize(Conversion.toBox2DXcoords(xSize), Conversion.toBox2DYcoords(ySize))
+  }
   
-  def currentTank: Tank = ???
+  def currentTank: Tank = this.tankList.current.get
   
   /**makes currentTank selection to forward by one*/
-  def nextTank: Unit = ???
+  def nextTank: Unit = this.tankList.nextItem()
   
   /**advances Box2D simulation with given time*/
-  def update(time: Double):Unit = ???
+  def update(time: Double):Unit = this.b2world.step(time.toFloat, World.VELCITER, World.POSITER)
   
   def addAmmunition(ammunition: Ammunition) = ???
   
