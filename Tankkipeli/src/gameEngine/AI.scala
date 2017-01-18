70a4b1bce2c74a665bc4cefab5fe8ba6d443cccf
markus.haikonen <markus.haikonen@SP3133.corp.supercell.net>
2017-01-18 22:44:22 +0900

    Added AI pseudo

--- Tankkipeli/src/gameEngine/AI.scala
+++ Tankkipeli/src/gameEngine/AI.scala
@@ -2,6 +2,74 @@
 
 package gameEngine
 
-object AI extends App {
+import scala.math
+
+
+class AI (val difficulty : Int){
+  var opponent_position = null
+  var ai_position = null
   
+  
+  def shoot{???} 
+  def target {???}
+
 }
+
+
+/* 
+in difficulty 
+
+
+-------------UI loops this!-------------
+ai move (ownposition, enemyposition)
+get remaining fuel
+get enemy direction
+	if enemy left
+	check if left clear if clear left
+	else if --> if clear right
+	else dont move
+	if enemy right --> samat jutut
+
+return (movedirection) 
+
+
+
+
+
+ai shoot (ownposition, enemyposition)
+get enemy direction left or right
+
+check magazine
+if empty move away---> ruuuuuun!!!
+
+
+select best weapon
+
+precalculate shoot 
+	if right 45deg
+	if left 135deg
+	set to var try_angle
+
+simulate shot (try_angle)
+	shoot(ownpos,tryangle, calculate needed power) 
+	if hit tryangle = shootangle
+	if miss tryangle adjust (difficulty)
+	
+	shoot(ownpos,tryangle + adjust , calculate needed power) 
+	shoot(ownpos,tryangle - adjust , calculate needed power)
+
+adjust = adjust + difficulty
+
+---> until hit
+if not hit shoot random :)
+
+Return shoot (ownpos, shootangle, powerneeded + randommodifier(difficulty))
+
+
+
+
+
+
+
+
+*/
